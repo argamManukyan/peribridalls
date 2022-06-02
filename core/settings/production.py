4980caadefda5017d64f172c844f649598d909dc
argam <manoukian00@gmail.com>
2022-06-02 15:00:09 +0400

    db config

--- core/settings/production.py
+++ core/settings/production.py
@@ -1,1 +1,12 @@
 from .settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DB_NAME'),
+        'HOST': config('DB_HOST'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'ATOMIC_REQUESTS': True,
+    }
+}
