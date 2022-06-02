582307a05fdf3f05bc5ba9f90bf27d38034b33ba
argam <manoukian00@gmail.com>
2022-06-02 17:36:00 +0400

    changes

--- shop/urls.py
+++ shop/urls.py
@@ -11,11 +11,11 @@ urlpatterns = [
     path('collection/<slug>/', product_details, name='product_details'),
     path('brands/', brand_list, name='brand_list'),
     path('categories/', category_list, name='category_list'),
-    # re_path('brands/(?P<slug>[\w-]+)/', brand_details, name='brand_details'),
-    # re_path('category/(?P<slug>[\w-]+)/', category_details, name='category_details'),
-    # re_path('search/', search, name='search'),
-    path('brands/<slug>/', brand_details, name='brand_details'),
-    path('category/<slug>/', category_details, name='category_details'),
-    path('search/', search, name='search'),
+    re_path('brands/(?P<slug>[\w-]+)/', brand_details, name='brand_details'),
+    re_path('category/(?P<slug>[\w-]+)/', category_details, name='category_details'),
+    re_path('search/', search, name='search'),
+    # path('brands/<slug>/', brand_details, name='brand_details'),
+    # path('category/<slug>/', category_details, name='category_details'),
+    # path('search/', search, name='search'),
 
 ]
