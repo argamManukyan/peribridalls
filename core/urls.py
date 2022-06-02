91e1e780e80b0dfedfde8205562168f009221c37
argam <manoukian00@gmail.com>
2022-06-02 15:59:46 +0400

    url router

--- core/urls.py
+++ core/urls.py
@@ -1,16 +1,16 @@
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls.static import static, settings
 
 import shop.views
 from wish.views import add_to_wish
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('add_to_wish/', add_to_wish, name='add_to_wish'),
-    path('ckeditor/', include('ckeditor_uploader.urls')),
-    path('rosetta/', include('rosetta.urls')),
+    re_path('admin/', admin.site.urls),
+    re_path('add_to_wish/', add_to_wish, name='add_to_wish'),
+    re_path('ckeditor/', include('ckeditor_uploader.urls')),
+    re_path('rosetta/', include('rosetta.urls')),
 ]
 
 urlpatterns += i18n_patterns(
