6c29cb1259cfe653def85bdcda5cd3a480b1da16
argam <manoukian00@gmail.com>
2022-06-03 00:34:58 +0400

    caching filter fix

--- core/settings/settings.py
+++ core/settings/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # thrid part plugins
-
+    'easy_select2',
     'ckeditor_uploader',
     'ckeditor',
     'singlemodeladmin',
